c959bbd54721d430f2e5123fe5e149f715c9b707
iCode <nalokonasalm@gmail.com>
2022-01-11 18:49:57 -0800

    Basic CRUD :) & and some node packages  inadvertently added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -24,19 +25,63 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+//allows for form data passing to route
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req, res) => {
-    //go to home.ejs
     res.render('home')
 })
 
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Background', description: "Cheap camping"})
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find()
+    res.render('campgrounds/index', { campgrounds }); 
 })
 
+
+//this route goes first before the route: '/campground/:id' as the
+//word "new" in this route is being treated as id. So order or 
+//route matters
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+
+//this route is called from new form {with method POST)
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground })
+})
+
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground })
+})
+
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)  
+})
+
+
+
+app.delete('/campgrounds/:id', async(req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 
 app.listen(3000, () => {
